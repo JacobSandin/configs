c8cbcd99aeb9bd9f11932094bea80c59d4f97e1a
Jacob Sandin <jacob@js.se>
2020-10-04 18:57:42 +0200

    Testing make

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -11,16 +11,15 @@ mkdir -p ~/.config/zsh
 
 rm -f ~/.z*
 
+# Using wget to get no-cashe curl did not handle this well.
 wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc -O ~/.zshrc  
-#curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
+wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim -O ~/.config/nvim/init.vim 
 
 # Oh my zsh setup
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
-wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim -O ~/.config/nvim/init.vim 
-#curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
 
 nvim -E -s -u "$HOME/.config/nvim/init.vim" +PlugInstall +qall
 
