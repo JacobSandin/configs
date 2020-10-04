fe78fde683997b47a029ef1bb5f2b01c8789c16d
Jacob Sandin <jacob@js.se>
2020-10-04 18:09:15 +0200

    Adding fzf

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -10,14 +10,14 @@ mkdir -p ~/.config/zsh
 
 
 rm ~/.z*
-curl -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
+curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
 
 # Oh my zsh setup
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
-curl -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
+curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
 
 nvim -E -s -u "$HOME/.config/nvim/init.vim" +PlugInstall +qall
 
