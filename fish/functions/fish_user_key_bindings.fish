a95b3ead704f04562dabab237dc037cde59ecba2
Jacob Sandin <jacob@js.se>
2020-10-04 22:15:47 +0200

    Cleanup

--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,9 +0,0 @@
-function fish_user_key_bindings
-  fish_vi_key_bindings
-  set fish_escape_delay_ms 100
-  bind -M insert -m default jj backward-char force-repaint
-end
-
-#function fish_user_key_bindings
-#fzf_key_bindings             
-#end                            
