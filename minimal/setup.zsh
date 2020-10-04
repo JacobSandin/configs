d72928210a1a00d861563b88e7c08bdf5ab80223
Jacob Sandin <jacob@js.se>
2020-10-04 17:31:55 +0200

    test

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -9,16 +9,16 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.config/zsh
 
 
+rm ~/.z*
+curl -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
+
 # Oh my zsh setup
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
-curl -fsSL https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
-curl -fsSL https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > ~/.zshrc
-
-rm ~/.z*
-source ~/.zshrc
+curl -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
 
 nvim -E -s -u "$HOME/.config/nvim/init.vim" +PlugInstall +qall
 
+. /home/jacsan/.zshrc
