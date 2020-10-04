5f96e76a856c219ec2bbabca0f071dd98723f20e
Jacob Sandin <jacob@js.se>
2020-10-04 22:14:54 +0200

    Cleanup

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -35,4 +35,5 @@ git config --global credential.helper cache
 # Set the cache to timeout after 1 hour (setting is in seconds)
 git config --global credential.helper 'cache --timeout=3600'
 
-
+git config --global user.email "jacob@js.se"
+git config --global user.name "Jacob Sandin"
