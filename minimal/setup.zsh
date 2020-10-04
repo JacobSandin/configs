4039596c1111b6d2c8535ddc10c58de9997c5ed8
JacobSandin <jacsan@vuprod-01.imcode.com>
2020-10-04 16:11:26 +0200

    Fixed the scripts to make first test

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 #
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/setup.zsh)"
+#
 # Change default shell to zsh
 chsh -s $(which zsh)
 # Create dirs
@@ -10,3 +12,7 @@ mkdir -p ~/.config/zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+curl -fsSL https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
+curl -fsSL https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > ~/.zshrc
+
