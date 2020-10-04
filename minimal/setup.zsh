87d4cd17913c94b5943b92babc8d1d28cf0fdfe4
Jacob Sandin <jacob@js.se>
2020-10-04 18:40:47 +0200

    Adding svim for sudo vim

--- minimal/setup.zsh
+++ minimal/setup.zsh
@@ -10,18 +10,21 @@ mkdir -p ~/.config/zsh
 
 
 rm -f ~/.z*
-curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
+
+wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc -O ~/.zshrc  
+#curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/zshrc > /home/jacsan/.zshrc
 
 # Oh my zsh setup
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
-curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
+wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim -O ~/.config/nvim/init.vim 
+#curl -H 'Cache-Control: no-cache' -fs https://raw.githubusercontent.com/JacobSandin/configs/master/minimal/init.vim > ~/.config/nvim/init.vim
 
 nvim -E -s -u "$HOME/.config/nvim/init.vim" +PlugInstall +qall
 
-. /home/jacsan/.zshrc
+source /home/jacsan/.zshrc
  
 # Set git to use the credential memory cache
 git config --global credential.helper cache
