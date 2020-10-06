d074170b8e7f03dcca533e240cb03fde164a90a1
Jacob Sandin <jacob@js.se>
2020-10-06 17:11:12 +0200

    created vim conf for, dev

--- development/setup.zsh
+++ development/setup.zsh
@@ -26,3 +26,5 @@ svn co https://svn.imcode.com/imcode/internal/Ansible Ansible
 
 wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/development/dev.vim -O ~/.config/nvim/dev.vim 
 wget --no-cache https://raw.githubusercontent.com/JacobSandin/configs/master/development/dev-plugin.vim -O ~/.config/nvim/dev-plugin.vim 
+
+nvim -E -s -u "$HOME/.config/nvim/init.vim" +PlugInstall +qall
