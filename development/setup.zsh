ac0b90b69d806efcc23e0fc0b3f806c1d941aa6a
Jacob Sandin <jacob@js.se>
2020-10-07 11:32:38 +0200

    CocInstall tests

--- development/setup.zsh
+++ development/setup.zsh
@@ -37,4 +37,6 @@ mkdir -p ~/.local/bin
 /home/jacsan/.cargo/bin/rustup component add rust-src                                                                                                              │Oct 06, 2020 9:45:42 PM org.apache.coyote.AbstractProtocol start
 /home/jacsan/.cargo/bin/cargo xtask install --server                                                                                                               │INFO: Starting ProtocolHandler ["ajp-bio-192.168.96.71-9276"]
 
+nvim +'CocInstall -sync coc-rust-analyzer' +qall
+nvim +CocUpdateSync +qall
 
