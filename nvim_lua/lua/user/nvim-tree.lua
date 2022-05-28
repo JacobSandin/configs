ef2c56940d4e34f2d5070341cf4aef05bff4ef02
Jacob testkonto <jacsan2@jswork-01.imcode.com>
2022-05-29 00:22:40 +0200

    nvim-tree and rainbow

--- nvim_lua/lua/user/nvim-tree.lua
+++ nvim_lua/lua/user/nvim-tree.lua
@@ -1,26 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -34,6 +13,30 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  renderer = {
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      }
+    }
+  },
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
@@ -42,7 +45,7 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = true,
+  --auto_close = true,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
@@ -86,15 +89,15 @@ nvim_tree.setup {
     number = false,
     relativenumber = false,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  --quit_on_open = 0,
+  --git_hl = 1,
+  --disable_window_picker = 0,
+  --root_folder_modifier = ":t",
+  --show_icons = {
+  --  git = 1,
+  --  folders = 1,
+  --  files = 1,
+  --  folder_arrows = 1,
+  --  tree_width = 30,
+  --},
 }
