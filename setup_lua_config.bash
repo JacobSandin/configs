21d4798e971228b87f0cb7e2c3b092d7cd85df91
Jacob testkonto <jacsan2@jswork-01.imcode.com>
2022-05-29 00:06:05 +0200

    syncscript and updated config

---
+++ setup_lua_config.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+if [[ ! -d ~/utv/git/config ]]; then
+  mkdir -p ~/utv/git
+  git clone https://github.com/JacobSandin/configs ~/utv/git/config
+else
+ cd ~/utv/git/config/
+ git pull
+fi
+if [[ ! -L ~/.config/nvim/lua ]] || [[ ! -e ~/.config/nvim/lua ]]; then
+  echo "LUA dir verkar tokigt skapar länkar"
+  rm -rf ~/.config/nvim/lua
+  ln -s ~/utv/git/config/nvim_lua/lua ~/.config/nvim/lua
+fi
+if [[ ! -L ~/.config/nvim/init.lua ]] || [[ ! -e ~/.config/nvim/init.lua ]]; then
+  rm -f ~/.config/nvim/init.lua
+  ln -s ~/utv/git/config/nvim_lua/init.lua ~/.config/nvim/init.lua
+  echo "init.lua verkar tokigt"
+fi
+
+
