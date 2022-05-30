8a603036a6a3c690c316960cef3af6e213cfeca9
Jacob testkonto <jacsan2@jswork-01.imcode.com>
2022-05-30 09:09:25 +0200

    Adding path if not set

--- setup_lua_config.bash
+++ setup_lua_config.bash
@@ -111,11 +111,11 @@ if [[ ! `command -v stylua` ]]; then
 fi
 
 #Python
-if [[ ! `command -v /home/jacsan/.local/bin/flake8` ]]; then
+if [[ ! `command -v flake8` ]]; then
   pip install flake8
 fi
 #Python
-if [[ ! `command -v /home/jacsan/.local/bin/black` ]]; then
+if [[ ! `command -v black` ]]; then
   pip install git+https://github.com/psf/black
 fi
 
