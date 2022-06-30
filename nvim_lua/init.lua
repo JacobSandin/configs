c3896b62e69ef50485859630a8a31779e152acba
Jacob testkonto <jacsan2@jswork-01.imcode.com>
2022-06-30 16:30:35 +0200

    Moving plugin

--- nvim_lua/init.lua
+++ nvim_lua/init.lua
@@ -1,7 +1,7 @@
 -- vim.cmd 'source ~/.config/nvim/init_old.vim'
+require "user.plugins"
 require "user.settings"
 require "user.keymaps"
-require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
 require "user.lsp"
