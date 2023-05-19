b927cc86b85adae95188a8bbd807d9a9d523ea87
Jacob Sandin <jacob@js.se>
2023-05-19 09:12:06 +0200

    No . for space in nvim and change RPROMPT

--- nvim_lua/lua/custom/init.lua
+++ nvim_lua/lua/custom/init.lua
@@ -7,5 +7,5 @@ g.shell="/bin/zsh"
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 vim.opt.list = true
 vim.opt.listchars = ""
-vim.opt.listchars:append "space:."
+-- vim.opt.listchars:append "space:."
 vim.opt.listchars:append "eol:↴"
