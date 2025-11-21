6b5eb1e39c2c8248edbfd901e7ca5cdad285d8f0
Jacob Sandin <jacob@js.se>
2025-11-21 12:50:09 +0100

    nvchad: Förbättra kontrast för diff-läge

--- nvchad_config/lua/chadrc.lua
+++ nvchad_config/lua/chadrc.lua
@@ -8,10 +8,21 @@ local M = {}
 M.base46 = {
 	theme = "ayu_light",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+		
+		-- Förbättrad kontrast för diff-läge
+		DiffAdd = { bg = "#d0f0d0", fg = "#005500", bold = true },
+		DiffChange = { bg = "#e0e8ff", fg = "#000080" },
+		DiffDelete = { bg = "#ffd0d0", fg = "#880000", bold = true },
+		DiffText = { bg = "#b0c8ff", fg = "#000080", bold = true },
+		
+		-- För bättre synlighet av ändringar
+		DiffAdded = { fg = "#006600", bold = true },
+		DiffRemoved = { fg = "#990000", bold = true },
+		DiffChanged = { fg = "#000099", bold = true },
+	},
 }
 
 M.nvdash = { load_on_startup = true }
